680c30270e5a7b22266ae4753dce13a772621b49
Saddam95 <65892245+Saddam95@users.noreply.github.com>
2021-02-26 14:25:57 +0300

    Delete pve.php

--- pve.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$file = fopen("pve.txt","at");
-fwrite($file,"\n $name:$surname \n");
-fclose($file);
-header('Location: http://b92453ko.beget.tech/loaderkeys/');
-?>
